e99091225880e90b463163256d74c2e2f71f2556
Alexander Urizar <urizaraj@gmail.com>
2018-02-24 20:04:55 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -24,12 +24,8 @@ end
 
 def can_be_created_in_a_block
   Movie.create do |m|
-    puts
-    puts
-    puts
-    puts m
-    puts
-    puts
+    m.title = 'Home Alone'
+    m.release_date = 1990
   end
 end
 
