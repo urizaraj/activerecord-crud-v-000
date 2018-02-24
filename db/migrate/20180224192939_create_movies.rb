d04a1f5047dde7e26a71dd8e80dba21bc4825037
Alexander Urizar <urizaraj@gmail.com>
2018-02-24 19:32:17 +0000

    automatically backed up by learn

--- db/migrate/20180224192939_create_movies.rb
+++ db/migrate/20180224192939_create_movies.rb
@@ -5,7 +5,7 @@ class CreateMovies < ActiveRecord::Migration
       t.timestamp :release_date
       t.string :director
       t.string :lead
-      t.boolean in_theaters
+      t.boolean :in_theaters
     end
   end
 end
