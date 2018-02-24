08def1ca4a7fd2d09912840062c6075572b405ec
Alexander Urizar <urizaraj@gmail.com>
2018-02-24 20:19:58 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -69,8 +69,8 @@ end
 def can_update_using_update_method
   # Update movie title to "Wat, huh?"
   Movie.create(title: "Wat?")
-  __
-  __
+  Movie.find_by(title: 'Wat?')
+  Movie.update(title: 'Wat, huh?')
 end
 
 def can_update_multiple_items_at_once
